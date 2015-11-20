7040c539fceebf4074c6b9494c6efe5ad0f70535
Kevin Hobbs <kevin21921@gmail.com>
2015-11-20 10:56:02 +0000

    Cleaned up code layout.

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -8,15 +8,14 @@ class FeedbacksController < ApplicationController
     @feedback = Feedback.new(feedback_params)
 
     if @feedback.save
-      
-      name = params[:feedback][:name]
+      name  = params[:feedback][:name]
       email = params[:feedback][:email]
-      body = params[:feedback][:comments]
-      
+      body  = params[:feedback][:comments]
       FeedbackMailer.feedback_email(name, email, body).deliver
-      
+
       flash[:success] = "Feedback Successfully Sent."
       redirect_to new_feedback_path
+   
     else
       flash[:danger] = "Error! Feedback Not Sent."
       redirect_to new_feedback_path
