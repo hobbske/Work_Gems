91f2a720fc50498d1f73b92894c095ea1abca360
Kevin Hobbs <kevin21921@gmail.com>
2015-11-22 17:21:11 +0000

    Added before filters in profiles and users controllers to authenticate permissions.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
 #   validates :first_name, presence: true
 #   validates :last_name, presence: true
 #   validates :company_name, presence: true
@@ -21,14 +23,32 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
-  
+
   def edit
     @user = User.find(params[:user_id] )
     @profile = @user.profile
   end
 
+  def update
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated Successfully!"
+      redirect_to user_path( params[:user_id] )
+    else
+      flash[:danger] = "Error. Please Resubmit. If you are not successful, please send feedback using the Feedback link in the navbar."
+      render action: :edit
+
+    end
+  end
+
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :company_name, :profession, :specialty, :job_title, :work_number, :mobile_number, :linkedin_url, :facebook_url, :description )
+    end
+  
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user      
     end
 end
