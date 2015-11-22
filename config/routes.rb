0c16fa253a6f51de02121e19089c22554a84831d
Kevin Hobbs <kevin21921@gmail.com>
2015-11-22 12:13:48 +0000

    Added profiles new action with link on homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, controllers: { registrations: 'users/registrations'}
   resources :users do
     resource :profile
   end
-  devise_for :users, controllers: { registrations: 'users/registrations'}
   resources :feedbacks
   get '/about' => 'pages#about'
   root 'pages#home'
