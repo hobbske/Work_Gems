1398d3714ea9bd86de519522cf1bb294889f1c29
Kevin Hobbs <kevin21921@gmail.com>
2015-12-18 01:44:29 -0500

    updated models to add thumb-size pics.

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -4,10 +4,10 @@ class Sample < ActiveRecord::Base
   validates :category, :title, :description, presence: true
   # validates :image_after, :presence => true, :if => "image_before.blank?"
 
-  has_attached_file :img_before, styles: { large: "500x500#", medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/img_placeholder.png"
+  has_attached_file :img_before, styles: { large: "500x500#", medium: "300x300#", small: "100x100#" }, default_url: "/images/:style/img_placeholder.png"
   validates_attachment_content_type :img_before, content_type: /\Aimage\/.*\Z/
 
-  has_attached_file :img_after, styles: { large: "500x500#", medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/img_placeholder.png"
+  has_attached_file :img_after, styles: { large: "500x500#", medium: "300x300#", small: "100x100#" }, default_url: "/images/:style/img_placeholder.png"
   validates_attachment_content_type :img_after, content_type: /\Aimage\/.*\Z/
 
 end
