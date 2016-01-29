f346f8f5df035935395643a2650a72b0261f6d2d
Kevin Hobbs <kevin21921@gmail.com>
2016-01-28 19:02:14 -0500

    Removed user_id column from sample table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151216021616) do
+ActiveRecord::Schema.define(version: 20160128235813) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.string   "name"
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 20151216021616) do
     t.boolean  "marquee"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
     t.string   "img_before_file_name"
     t.string   "img_before_content_type"
     t.integer  "img_before_file_size"
@@ -67,8 +66,6 @@ ActiveRecord::Schema.define(version: 20151216021616) do
     t.integer  "img_after_file_size"
     t.datetime "img_after_updated_at"
   end
-
-  add_index "samples", ["user_id"], name: "index_samples_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
