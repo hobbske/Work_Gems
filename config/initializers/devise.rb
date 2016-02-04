18d427ed2efbec310e4a08021cb89995bfc6e4fd
Kevin Hobbs <kevin21921@gmail.com>
2016-02-04 12:29:22 -0500

    Added initializer configurations to devise.rb for twitter, facebook and linkedin, with placeholder key and secret.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -236,6 +236,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :twitter, ENV["TWITTER CONSUMER KEY"], ENV["TWITTER CONSUMER SECRET"]
+  config.omniauth :facebook, ENV["FACEBOOK CONSUMER KEY"], ENV["FACEBOOK CONSUMER SECRET"]
+  config.omniauth :linkedin, ENV["LINKEDIN CONSUMER KEY"], ENV["LINKEDIN CONSUMER SECRET"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
