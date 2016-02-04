8367f62370339132c26e2b4548ab837a6987a7d0
Kevin Hobbs <kevin21921@gmail.com>
2016-02-04 12:52:02 -0500

    Added provider and user id columns to user schema and migrated.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129002920) do
+ActiveRecord::Schema.define(version: 20160204175001) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20160129002920) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "identities", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
 
   create_table "plans", force: :cascade do |t|
     t.string   "name"
@@ -83,6 +93,9 @@ ActiveRecord::Schema.define(version: 20160129002920) do
     t.datetime "updated_at",                          null: false
     t.integer  "plan_id"
     t.string   "stripe_customer_token"
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
