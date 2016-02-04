f3739488a659818b2a34ce1bfec6e8db2d9f3c37
Kevin Hobbs <kevin21921@gmail.com>
2016-02-04 12:48:52 -0500

    Added Devise::OmniauthCallbacksController inheritance to the OmniauthCallbacksController and created an all action for providors.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,2 +1,10 @@
-class OmniauthCallbacksController < ApplicationController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+	def all
+		user = User.from_omniauth(request.env["omniaut.auth"])
+	end
+	alias_method :twitter, :all
+	alias_method :facebook, :all
+	alias_method :linkedin, :all
+
+
 end
