9eeae31d137ca86dd173de0de9febe65a234cde4
Kevin Hobbs <kevin21921@gmail.com>
2016-02-04 13:14:26 -0500

    Added 'all ' action to omniauth_controller to redirect the user based on success or failure, added methods to user.rb file and modified input forms to hide password if when logging in via omniauth.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,12 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def all
 		user = User.from_omniauth(request.env["omniaut.auth"])
+		if user.persisted?
+			sign_in_and_redirect user, notice: "Signed in!"
+		else
+			sesson["devise.user_attributes"] = user.attributes
+			redirect_to new_user_registration_url
+		end
 	end
 	alias_method :twitter, :all
 	alias_method :facebook, :all
